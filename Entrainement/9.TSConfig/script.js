31ec028cc8b8fb1be5f87ffc364f35fae7d06e09
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-04-03 12:08:26 +0200

    Nouveaux enseignements

---
+++ Entrainement/9.TSConfig/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let prenom = "francis";
+const array = [1, 2, 3];
+const array2 = [...array, 4, 5, 6];
+console.log(array2);
---
+++ Entrainement/9.TSConfig/script.ts
@@ -0,0 +1,4 @@
+let prenom = "francis"
+const array = [1, 2, 3]
+const array2 = [...array, 4, 5, 6]
+console.log(array2);
